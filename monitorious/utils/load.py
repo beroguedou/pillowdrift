b67fc695d9a0f59ccae75be21455303208068fdf
Béranger <beranger@pillowanalytica.com>
2022-08-23 21:01:55 +0200

    Data and config loading are ok

---
+++ monitorious/utils/load.py
@@ -0,0 +1,28 @@
+
+import yaml 
+import pandas as pd
+
+
+def load_config(configpath):
+    with open(configpath, 'r') as file:
+        content = yaml.safe_load(file)
+    return content
+
+
+def load_data_from_csv(datapath):
+    dataframe = pd.read_csv(datapath)
+
+    #categorical = list(config['model']['variables']['categorical'].keys())
+    #ordinal = config['model']['variables']['numerical']['ordinal']
+    data = []
+    columns = dataframe.columns.tolist()
+    for col in columns:
+        data.append(dataframe[col].values.tolist())
+    return data, columns
+    
+
+def load_data_from_mysql():
+    pass
+
+def load_data_from_postgresql():
+    pass
