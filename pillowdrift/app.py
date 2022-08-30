f5b1133e7de48b7225c65861687edcfbb7f0e08d
Béranger <beranger@pillowanalytica.com>
2022-08-30 16:36:56 +0200

    Some acceleration due to the modification of apis

--- pillowdrift/app.py
+++ pillowdrift/app.py
@@ -10,10 +10,6 @@ from pillowdrift.utils.logging_config import logging_config
 config.dictConfig(logging_config)
 logger = getLogger('root')
 
-#logger.info("Informative messages from your code.")
-#logger.warning("Everything works but there is something to be aware of.")
-#logger.error("There's been a mistake with the process.")
-#logger.critical("There is something terribly wrong and process may terminate.")
 
 parser = argparse.ArgumentParser(
     description=' Helps to turn on the monitoring dashboards .')
